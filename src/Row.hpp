07994ec3850da55957f6680be62ee3972118184a
Ashwin-Rajesh <aswinrajesh2@gmail.com>
2020-06-03 19:23:21 +0530

    Added files

---
+++ src/Row.hpp
@@ -0,0 +1,13 @@
+#ifndef __ROW__
+#define __ROW__
+
+namespace sudoku{
+    class row{
+    private:
+
+    public:
+        
+    }
+}
+
+#endif
---
+++ src/Sudoku.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Front-end under development, Nothing to see here!";
+}
